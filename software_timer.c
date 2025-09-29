328b235551933726f71ad528effcb708458b6df2
thanhhuytran124 <thanhhuytran124@gmail.com>
2025-09-29 17:21:07 +0700

    Add files via upload

---
+++ software_timer.c
@@ -0,0 +1,29 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Sep 28, 2025
+ *      Author: HP
+ */
+#include "software_timer.h"
+int timer1_counter = 0 ;
+int timer1_flag = 0 ;
+int timer2_counter = 0 ;
+int timer2_flag = 0 ;
+void setTimer1( int duration) {
+	timer1_counter = duration;
+	timer1_flag = 0;
+
+}
+
+void timerRun()  {
+	if (timer1_counter > 0 ) {
+		timer1_counter--;
+		if (timer1_counter <= 0 ){
+			timer1_flag = 1;
+		}
+	}
+
+}
+
+
+
