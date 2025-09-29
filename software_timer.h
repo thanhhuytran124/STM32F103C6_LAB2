71872ec204316ecf0e9437b8c42f2b80a9bbdf2c
thanhhuytran124 <thanhhuytran124@gmail.com>
2025-09-29 18:02:43 +0700

    Add files via upload

---
+++ software_timer.h
@@ -0,0 +1,19 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 28, 2025
+ *      Author: HP
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+extern int timer1_flag;
+void setTimer1(int duration);
+extern int timer2_flag;
+void setTimer2(int duration1);
+extern int timer3_flag;
+void setTimer3(int duration);
+void timerRun();
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
